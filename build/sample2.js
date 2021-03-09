3fcf24ea3fd282b13cb66c02c66c861f2a078d52
AbdulMoiz8994 <moiza8994@gmail.com>
2021-03-09 15:25:34 +0500

    add these practice of typescript

--- build/sample2.js
+++ build/sample2.js
@@ -1,2 +1,14 @@
 "use strict";
 console.log("hello World");
+var a = 1 + 2;
+var b = a + 3;
+var c = {
+    apple: a,
+    banana: b,
+};
+var d = c.apple * 4;
+console.log(d);
+function square(n) {
+    return n * n;
+}
+console.log(square(0));
--- src/sample2.ts
+++ src/sample2.ts
@@ -1,1 +1,16 @@
 console.log("hello World")
+
+
+let a=1+2;
+let b=a+3;
+let c={
+    apple: a,
+    banana: b,
+}
+let d=c.apple * 4;
+console.log(d);
+
+function square(n:number) {
+    return n*n
+}
+console.log(square(0))

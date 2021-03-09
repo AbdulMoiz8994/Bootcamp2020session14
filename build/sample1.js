d23ea6e52d55e8fc9cffb7f3001cbaa60ebc67d8
AbdulMoiz8994 <moiza8994@gmail.com>
2021-03-09 12:36:17 +0500

    add these filder and folder of typescript

---
+++ build/sample1.js
@@ -0,0 +1,39 @@
+"use strict";
+// console.log("Hello World!")
+// let b=20;
+// const obj={};
+// console.log(obj.name);
+// function a(b) {
+//     return b*20;
+// }
+// a(10);
+// function c(b:number) {
+//     return b*20;
+// }
+// c(10);
+// const firstName: string= "Abdul";
+// const lastName: string="moiz";
+// const age: number=20;
+// const isPresent: boolean=true;
+// const array: boolean[]=[true,false,true];
+// const obj: Object={
+//     names: "moiz",
+//     age: 12,
+// }
+// const result: string=firstName + lastName;
+// console.log(result);
+// const fullName: number= firstName + age
+// const objects:  object={
+//     names: "khanzada",
+//     age: 25,
+// }
+// console.log({objects})
+// In array we must specify the type
+//This is object of array 
+// const arrays:  object[]=[{names: "moiz"}];
+// console.log(arrays)
+document.write("Hello World we are on TS file");
+function isEven(num) {
+    return num % 2 === 0;
+}
+console.log(isEven(12));
